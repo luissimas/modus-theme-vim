df07cebe86d38030c57d3e6e979096a289c00e89
Luis Simas <luissimas@protonmail.com>
2021-11-26 09:55:36 -0300

    lualine themes

--- lua/lualine/themes/modus-vivendi.lua
+++ lua/lualine/themes/modus-vivendi.lua
@@ -1,33 +1,37 @@
-local colors = require("colors.modus-vivendi")
+local colors = require("modus-themes.colors.modus-vivendi")
+
+for k, v in pairs(colors) do
+	colors[k] = v[1]
+end
 
 return {
 	normal = {
-		a = { bg = colors.blue, fg = colors.bg_main, gui = 'bold' },
+		a = { bg = colors.blue, fg = colors.bg_main, gui = "bold" },
 		b = { bg = colors.bg_alt, fg = colors.blue },
 		c = { bg = colors.bg_dim, fg = colors.fg_main },
 	},
 	insert = {
-		a = { bg = colors.green, fg = colors.bg_main, gui = 'bold' },
+		a = { bg = colors.green, fg = colors.bg_main, gui = "bold" },
 		b = { bg = colors.bg_alt, fg = colors.green },
 		c = { bg = colors.bg_dim, fg = colors.fg_main },
 	},
 	visual = {
-		a = { bg = colors.magenta, fg = colors.bg_main, gui = 'bold' },
+		a = { bg = colors.magenta, fg = colors.bg_main, gui = "bold" },
 		b = { bg = colors.bg_alt, fg = colors.magenta },
 		c = { bg = colors.bg_dim, fg = colors.fg_main },
 	},
 	replace = {
-		a = { bg = colors.red, fg = colors.bg_main, gui = 'bold' },
+		a = { bg = colors.red, fg = colors.bg_main, gui = "bold" },
 		b = { bg = colors.bg_alt, fg = colors.red },
 		c = { bg = colors.bg_dim, fg = colors.fg_main },
 	},
 	command = {
-		a = { bg = colors.yellow, fg = colors.bg_main, gui = 'bold' },
+		a = { bg = colors.yellow, fg = colors.bg_main, gui = "bold" },
 		b = { bg = colors.bg_alt, fg = colors.yellow },
 		c = { bg = colors.bg_dim, fg = colors.fg_main },
 	},
 	inactive = {
-		a = { bg = colors.bg_inactive, fg = colors.fg_inactive, gui = 'bold' },
+		a = { bg = colors.bg_inactive, fg = colors.fg_inactive, gui = "bold" },
 		b = { bg = colors.bg_inactive, fg = colors.fg_inactive },
 		c = { bg = colors.bg_inactive, fg = colors.fg_inactive },
 	},
